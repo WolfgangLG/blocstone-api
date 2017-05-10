a342e6418cd8a3f5fbae29dc8a8f9766f15bf46f
Ludwig Gerdes <lgerdes@g2crowd.com>
2017-05-10 08:26:00 -0500

    Customize private user profile pagewith edit partial

--- spec/views/profiles/show.html.erb_spec.rb
+++ spec/views/profiles/show.html.erb_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "profiles/show", type: :view do
-  before(:each) do
-    @profile = assign(:profile, Profile.create!(
-      :show => "Show"
-    ))
-  end
-
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Show/)
-  end
+  
 end
