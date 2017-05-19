deec66b6f7e0d23563ae0d20a099150278a3b4e1
Ludwig Gerdes <lgerdes@g2crowd.com>
2017-05-19 06:00:01 -0500

    Change glassdoor authentication token referenes to partner id and key fields

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -40,12 +40,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :g2_crowd_api_token, :mattermark_api_token, :glassdoor_api_token])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :g2_crowd_api_token, :mattermark_api_token, :glassdoor_partner_id, :glassdoor_key])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :g2_crowd_api_token, :mattermark_api_token, :glassdoor_api_token])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :g2_crowd_api_token, :mattermark_api_token, :glassdoor_partner_id, :glassdoor_key])
   end
 
   # The path used after sign up.
