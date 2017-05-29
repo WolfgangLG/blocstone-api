80ee36d4c7b4f26d606af326bf7a950ddc9c4248
Ludwig Gerdes <lgerdes@g2crowd.com>
2017-05-28 20:55:24 -0500

    Update glassdoor service to persist api responses to database

--- app/services/glassdoor_service.rb
+++ app/services/glassdoor_service.rb
@@ -4,16 +4,45 @@ class GlassdoorService
   include HTTParty
 
   def initialize(user)
-    @glassdoor_partner = user.glassdoor_partner_id
-    @glassdoor_key = user.glassdoor_key
+    @user = user
   end
 
   # Find company on Glassdoor by name
   def find_employer(company, options = {})
-    api_url = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=#{ @glassdoor_partner }&t.k=#{ @glassdoor_key }"
+    api_url = "http://api.glassdoor.com/api/api.htm?v=1&format=json&t.p=#{ glassdoor_partner }&t.k=#{ glassdoor_key }"
     url = "#{ api_url }&action=employers&ps=1&q=#{ company }"
     response = HTTParty.get(url, options)
     json = JSON.parse(response.body)
-    json["response"]["employers"][0]
+    single_employer = json["response"]["employers"]
+
+    gd_employer = single_employer.map do |atr|
+      gd = GlassdoorEmployer.new
+      gd.employer_id = atr["id"]
+      gd.name = atr["name"]
+      gd.website = atr["website"]
+      gd.industry = atr["industry"]
+      gd.squareLogo = atr["squareLogo"]
+      gd.numberOfRatings = atr["numberOfRatings"]
+      gd.overallRating = atr["overallRating"]
+      gd.ratingDescription = atr["ratingDescription"]
+      gd.cultureAndValuesRating = atr["cultureAndValuesRating"]
+      gd.seniorLeadershipRating = atr["seniorLeadershipRating"]
+      gd.compensationAndBenefitsRating = atr["compensationAndBenefitsRating"]
+      gd.careerOpportunitiesRating = atr["careerOpportunitiesRating"]
+      gd.workLifeBalanceRating = atr["workLifeBalanceRating"]
+      gd.save!
+      gd
+    end
+    gd_employer.select(&:persisted?)
+  end
+
+  private
+
+  def glassdoor_partner
+    @glassdoor_partner = @user.glassdoor_partner_id
+  end
+
+  def glassdoor_key
+    @glassdoor_key = @user.glassdoor_key
   end
 end
